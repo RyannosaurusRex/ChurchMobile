28651b99e6ba652e5c3b55f6d6e1b07e4b4923c8
Ryan Hayes <ryanhayes84@gmail.com>
2011-07-30 21:55:34 -0400

    First commit

---
+++ src/net/ryanhayes/MainActivity.java
@@ -0,0 +1,17 @@
+package net.ryanhayes;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.phonegap.DroidGap;
+
+public class MainActivity extends DroidGap
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        //super.loadUrl("file:///android_asset/www/index.html");
+        super.loadUrl("http://ryanhayes.net");
+    }
+}
